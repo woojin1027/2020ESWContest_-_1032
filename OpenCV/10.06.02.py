19d1774a1079a90a31dd00aee01542a4bc194cc4
woojin1027 <woojinny@kakao.com>
2020-10-06 18:58:09 +0900

    영상 땜시 만드는 거

---
+++ OpenCV/10.06.02.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+body_cascade = cv2.CascadeClassifier('../../main/OpenCV/haarcascade/haarcascade_fullbody.xml')
+image = cv2.imread('people.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+body = body_cascade.detectMultiScale(gray, 1.01, 2, minSize=(500, 500))
+num = 0
+for (x,y,w,h) in body :         
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),30)
+    num += 1
+
+image = cv2.resize(image,(500,600))
+print('사진에서 보이는 인원 = ',num)
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
